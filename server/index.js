59ac968d93f5ab309389b1142be295c20bb8c30f
unknown <dullabop@gmail.com>
2021-02-16 15:57:51 -0500

    Server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejs = require ('ejs');
+const path = require('path');
+
+const clientPath = path.join(__dirname,'../client');
+const staticPath = path.join(clientPath,'/static');
+const viewsPath = path.join(clientPath,'/views');
+
+const app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile(staticPath+'/index.html');
+});
+
+app.get('/favorites', function(req, res) {
+	res.sendFile(staticPath+'/Favorites.html');
+});
+
+app.use(express.static(staticPath));
+
+
+app.listen(2000);
