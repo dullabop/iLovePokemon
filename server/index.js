a55b3a926afa931a1fc8a29ea2bd38c7cebca10a
unknown <dullabop@gmail.com>
2021-03-01 15:06:56 -0500

    tutorial 1

--- server/index.js
+++ server/index.js
@@ -2,21 +2,33 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
 
-const clientPath = path.join(__dirname,'../client');
-const staticPath = path.join(clientPath,'/static');
-const viewsPath = path.join(clientPath,'/views');
+const clientPath = path.join(__dirname,'../client/');
+const staticPath = path.join(clientPath,'/static/');
+const viewsPath = path.join(clientPath,'/views/');
 
 const app = express();
 
-app.get('/', function(req, res) {
-	res.sendFile(staticPath+'/index.html');
-});
+app.set('view engine','ejs');
+app.set('views',viewsPath);
 
-app.get('/favorites', function(req, res) {
-	res.sendFile(staticPath+'/Favorites.html');
-});
+var x = 0;
+
+const counter = function(req, res, next) {
+	x++;
+	console.log(x);
+	next();
+}
+//used to activate the counter
+app.use(counter);    
 
 app.use(express.static(staticPath));
 
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/favorites', counter, function(req, res) {
+	res.render('famous', {count=});
+});
 
 app.listen(2000);
