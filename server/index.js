4c687c4c10ffadc073e6b33919a2bbf86a3cb3ad
unknown <dullabop@gmail.com>
2021-03-14 22:35:04 -0400

    3rd TUT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
+const bodyParser= require('body-parser');
+const session = require('express.session');
 
 //Navigation
 
@@ -8,31 +10,40 @@ const clientPath = path.join(__dirname,'../client/');
 const staticPath = path.join(clientPath,'/static/');
 const viewsPath = path.join(clientPath,'/views/');
 
+//Basic Server
+
 const app = express();
 app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+		name: 'pokemons'
+		secret: 'eachpokehad7pokemons'
+		saveUninitialized: false,
+		resave: false,
+		cookie: {
+			maxAge: 1000*60*60*24*3,
+		}
+}))
+
 app.listen(2000);
+
+//Setting Views
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
 
-//Vistor Counter
+//Routes
 
-var x = 0;
-
-const counter = function(req, res, next) {
-	x++;
-	console.log(x);
-	next();
-}
-//used to activate the counter
-app.use(counter);    
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', {data: req.session});
 });
 
-app.get('/favorites', counter, function(req, res) {
-	res.render('famous');
+app.get('/Favorites', counter, function(req, res) {
+	res.render('Favorites', {data: req.session});
 });
 
-
+app.post('/welcome', (req, res) => {
+	req.session.username=req.body.nombre;
+	res.send('SUCCESS');
+});
